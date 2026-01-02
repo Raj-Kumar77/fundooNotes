5bd70aa6c672407cade12151343fbd924c3aa56b
Raj <raj123kumar0123@gmail.com>
2026-01-02 12:22:08 +0530

    api for label made

--- backend/controller/labelController.js
+++ backend/controller/labelController.js
@@ -22,4 +22,75 @@ const createLabel = async (req, res) => {
     }
 };
 
-export { createLabel };
+const getLabel = async(req, res) => {
+    try {
+
+        const {id} = req.params
+
+        const label = await Label.findById({_id:id, user:req.user.id})
+        if(!label){
+            res.status(404).json({success: false, message: 'Label not found or unauthorized'})
+        }
+
+        return res.status(200).json({success: true, message: 'Lable found', label})
+        
+    } catch (error) {
+         console.error(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+const getAllLabels = async (req, res) => {
+    try {
+
+        const labels = await Label.find({ user: req.user.id })
+        res.status(200).json({ success: true, message: 'All labels', labels })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+const updateLabel = async (req, res) => {
+    try {
+
+        const {id} = req.params
+        const {name, color} = req.body
+
+        const updateData = {}
+        if(name) updateData.name = name
+        if(color) updateData.color = color 
+
+        const updatedLabel = await Label.findOneAndUpdate({_id:id, user: req.user.id}, updateData, {new: true})
+        if(!updatedLabel){
+            res.status(404).json({success: false, message: 'Lable not found or not authorize'})
+        }
+
+        res.status(200).json({success: true, message: 'Label updated', updatedLabel})
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+const deleteLabel = async(req, res) => {
+    try {
+
+        const {id} = req.params
+
+        const deletedLabel = await Label.findOneAndDelete({_id:id, user:req.user.id})
+        if(!deleteLabel){
+            res.status(404).json({success: false, message: 'Label not found or unautorized'})
+        }
+
+        res.status(200).json({success: true, message: 'Label deleted', deletedLabel})
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
+
+export { createLabel, getAllLabels, updateLabel, getLabel, deleteLabel };
--- backend/route/labelRoute.js
+++ backend/route/labelRoute.js
@@ -1,9 +1,13 @@
 import express from 'express'
-import { createLabel } from '../controller/labelController.js'
+import { createLabel, deleteLabel, getAllLabels, getLabel, updateLabel } from '../controller/labelController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
 
 const labelRouter = express.Router()
 
-labelRouter.post('/create', authMiddleware, createLabel)
+labelRouter.post('/', authMiddleware, createLabel)
+labelRouter.get('/', authMiddleware, getAllLabels)
+labelRouter.get('/:id', authMiddleware, getLabel)
+labelRouter.patch('/:id', authMiddleware, updateLabel)
+labelRouter.delete('/:id', authMiddleware, deleteLabel)
 
 export default labelRouter
